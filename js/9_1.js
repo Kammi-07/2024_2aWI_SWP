3dc2ad102a75e581fb1cb95f09cd944cc93a3c18
Pius Kammerlander <pius.kammerlander@student.htldornbirn.at>
2025-02-04 21:30:42 +0100

    04.12comit01

---
+++ js/9_1.js
@@ -0,0 +1,17 @@
+console.log("console funktioniert");
+let x = 0;
+while (x = 223) {
+    if (x % 5 === 0) {
+        console.log("Foo");
+    }
+    if (x % 7 === 0) {
+        console.log(Bar);
+    }
+    if (x % 5 === 0 && x % 7 === 0) {
+
+    }
+    else {
+        console.log(x);
+    }
+    x = x + 1;
+}
