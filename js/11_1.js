ead7c114c2aacd4947c3be77ad1bf5b9db094524
Pius Kammerlander <pius.kammerlander@student.htldornbirn.at>
2025-03-28 13:36:04 +0100

    28_03_25commit02

--- js/11_1.js
+++ js/11_1.js
@@ -1,31 +1,37 @@
 let input = "AACT";
-let acount = 0;
-let ccount = 0;
-let gcount = 0;
-let tcount = 0;
+
+function countlikedna(input) {
 
 
-for (let index = 0; index < input.length; index++) {
-    if (input[index] == "A") {
-        acount++;
-    } else if (input[index] == "C") {
-        ccount++;
-    } else if (input[index] == "g") {
-        gcount++;
+    let acount = 0;
+    let ccount = 0;
+    let gcount = 0;
+    let tcount = 0;
 
-    } else if (input[index] == "T") {
-        tcount++;
+
+    for (let index = 0; index < input.length; index++) {
+        if (input[index] == "A") {
+            acount++;
+        } else if (input[index] == "C") {
+            ccount++;
+        } else if (input[index] == "G") {
+            gcount++;
+
+        } else if (input[index] == "T") {
+            tcount++;
+
+        }
+
 
     }
+    let acounntstr = String(acount);
+    let ccountstr = String(ccount);
+    let gcountstr = String(gcount);
+    let tcountstr = String(tcount);
 
+
+
+    console.log(acounntstr + " " + ccountstr + " " + gcountstr + " " + tcountstr);
 
 }
-let acounntstr = String(acount);
-let ccountstr = String(ccount);
-let gcountstr = String(gcount);
-let tcountstr = String(tcount);
-
-
-
-console.log(acounntstr + " " + ccountstr + " " + gcountstr + " " + tcountstr);
-
+countlikedna(input)
