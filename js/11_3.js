40ca22229db61aab68b3c865596b26bdc02b5074
Pius Kammerlander <pius.kammerlander@student.htldornbirn.at>
2025-04-04 13:59:22 +0200

    4.4.2025comit02

--- js/11_3.js
+++ js/11_3.js
@@ -14,15 +14,20 @@ function Betrag(zahl) {
 }
 console.log(Betrag(-1) + " " + Betrag(4) + " " + Betrag(-16) + " " + Betrag(0));
 function nearerby0(betragnumber, numbers) {
-    let neareestnumber = 0;
-    let neareestnumberarray = 0;
-    for (let index = 0; index < Betragnumbers.length; index++) {
-        if (betragnumber[index] < betragnumber[index - 1]) {
-            neareestnumber = numbers[index]
-            neareestnumberarray = index;
-        } else if (condition) {
+    let nearestNumber = numbers[0];
+    let nearestBetrag = betragnumber[0];
 
+    for (let index = 1; index < betragnumber.length; index++) {
+        if (betragnumber[index] < nearestBetrag) {
+            nearestBetrag = betragnumber[index];
+            nearestNumber = numbers[index];
+        } else if (betragnumber[index] === nearestBetrag) {
+            if (numbers[index] > nearestNumber) {
+                nearestNumber = numbers[index];
+            }
         }
     }
 
+    return nearestNumber;
 }
+console.log(nearerby0(Betragnumbers, numbers));
